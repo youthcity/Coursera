dbc5eef91e54e75664e07085dab38ec7583f9943
youthcity <397731024@qq.com>
2016-06-16 23:22:34 +0800

    add ios3

--- IOS应用开发技术/Ios_01_20150506.m
+++
@@ -1,7 +0,0 @@
-#import<Foundataion/Foundation.h>
-
-int main(int argc, const char *argv[]){
-	NSlog(@"Hello World!");
-	return 0;
-}
-
---
+++ IOS应用开发技术/第十次课/第10次课/练习/01 联系人管理/01 联系人管理/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01 联系人管理
+//
+//  Created by xuhui on 16/5/31.
+//  Copyright © 2016年 xuhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
